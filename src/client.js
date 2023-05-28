05147a9aba757cf845b209c7c1b8f227793ede80
Tautvilas Mečinskas <connect@tautvilas.lt>
2023-05-28 11:28:32 +0300

    pubsub hello world

--- src/client.js
+++ src/client.js
@@ -4,6 +4,7 @@ import { gossipsub } from '@chainsafe/libp2p-gossipsub'
 import { tcp } from '@libp2p/tcp'
 import { noise } from '@chainsafe/libp2p-noise'
 import { mplex } from '@libp2p/mplex'
+import { yamux } from '@chainsafe/libp2p-yamux'
 import { multiaddr } from 'multiaddr'
 import { fromString as uint8ArrayFromString } from "uint8arrays/from-string";
 
@@ -11,8 +12,18 @@ const client = async () => {
   const node = await createLibp2p({
     transports: [tcp()],
     connectionEncryption: [noise()],
-    streamMuxers: [mplex()],
+    streamMuxers: [mplex(), yamux()],
     pubsub: gossipsub({ allowPublishToZeroPeers: true })
+  })
+
+  node.addEventListener('peer:connect', async (evt) => {
+    console.log('Connection established to:', evt.detail.remotePeer.toString())	// Emitted when a new connection has been created
+    console.log((await node.peerStore.all()).length);
+  })
+
+  node.addEventListener('peer:discovery', (evt) => {
+    // No need to dial, autoDial is on
+    console.log('Discovered:', evt.detail.id.toString())
   })
 
   if (process.argv.length >= 3) {
@@ -20,13 +31,18 @@ const client = async () => {
     console.log(`pinging remote peer at ${process.argv[2]}`)
     const latency = await node.ping(ma)
     console.log(`pinged ${process.argv[2]} in ${latency}ms`)
-    const resp = await node.pubsub.publish('news', uint8ArrayFromString('banana'))
-    console.log(resp);
   } else {
     console.log('no remote peer address given, skipping ping')
   }
 
-  await node.stop()
+  setInterval(async () => {
+    const resp = await node.pubsub.publish('news', uint8ArrayFromString('Bird bird bird, bird is the word!')).catch(err => {
+      console.error(err)
+    })
+    console.log(resp)
+  }, 1000)
+
+  // await node.stop()
 }
 
 export default client
